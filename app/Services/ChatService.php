6320b5c7bfe16a72a00478a1a9baacc357e23ddd
João Lucas Buzzo <108816555+JoBuzzo@users.noreply.github.com>
2023-11-16 20:51:00 -0300

    ChatUser::withTrashed

--- app/Services/ChatService.php
+++ app/Services/ChatService.php
@@ -7,6 +7,7 @@ use App\Models\Chat;
 use App\Models\ChatUser;
 use App\Models\Message;
 use Illuminate\Http\Request;
+
 class ChatService
 {
 
@@ -33,40 +34,43 @@ class ChatService
     {
         $chat = Chat::with(['users'])->find($id);
 
-        if($chat->users->contains($user_id)){
-            if(!$chat->name && $chat->users->count() <= 2) {
+        if ($chat->users->contains($user_id)) {
+            if (!$chat->name && $chat->users->count() <= 2) {
                 foreach ($chat->users as $user) {
-                    if($user->id != $user_id) {
+                    if ($user->id != $user_id) {
                         $chat->name = $user->name;
                         break;
                     }
                 }
             }
-        }else {
+        } else {
             return response()->json(['Error' => 'Você não tem permição para acessar esse chat']);
         }
 
-        $messages = Message::where('messages.chat_id', $chat->id)
-            ->leftJoin('users', 'messages.user_id', '=', 'users.id')
-            ->leftJoin('messages as parent', 'messages.parent_id', '=', 'parent.id')
-            ->leftJoin('users as parent_user', 'parent.user_id', '=', 'parent_user.id')
-            ->select(
-                'users.name as user_name',
-                'messages.user_id',
-                'messages.id as message_id',
-                'messages.content',
-                'messages.media',
-                'messages.sent_at',
-                'messages.parent_id as parent_message_id',
-                'parent_user.id as parent_user_id',
-                'parent_user.name as parent_user_name',
-                'parent.content as parent_content',
-            )
-            ->orderBy('messages.sent_at', 'asc')
-            ->get();
+        if (ChatUser::withTrashed()->where('user_id', $user_id)->where('chat_id', $id)->first()->deleted_at == null) {
+            $messages = Message::where('messages.chat_id', $chat->id)
+                ->leftJoin('users', 'messages.user_id', '=', 'users.id')
+                ->leftJoin('messages as parent', 'messages.parent_id', '=', 'parent.id')
+                ->leftJoin('users as parent_user', 'parent.user_id', '=', 'parent_user.id')
+                ->select(
+                    'users.name as user_name',
+                    'messages.user_id',
+                    'messages.id as message_id',
+                    'messages.content',
+                    'messages.media',
+                    'messages.sent_at',
+                    'messages.parent_id as parent_message_id',
+                    'parent_user.id as parent_user_id',
+                    'parent_user.name as parent_user_name',
+                    'parent.content as parent_content',
+                )
+                ->orderBy('messages.sent_at', 'asc')
+                ->get();
 
+                return response()->json(['chat' => $chat, 'messages' => $messages]);
+            }
 
-        return response()->json(['chat' => $chat, 'messages' => $messages]);
+        return response()->json(['chat' => $chat, 'messages' => []]);
     }
 
     public static function store(Request $request)
@@ -91,14 +95,13 @@ class ChatService
 
     public static function destroy($id, $user_id)
     {
-        if($chatUser = ChatUser::where('chat_id', '=', $id)->where('user_id', $user_id)->first()){
+        if ($chatUser = ChatUser::where('chat_id', '=', $id)->where('user_id', $user_id)->first()) {
 
             $chatUser->delete();
 
             return response()->json(['success' => true]);
         }
-        
-        return response()->json(['success' => false]);
 
+        return response()->json(['success' => false]);
     }
 }
